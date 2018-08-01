3b72d028ab3ef22d9932177303cd938e0700ed2a
Saeed Kola <mohammedsaeedk@gmail.com>
2018-08-01 13:48:44 +0530

    restore old working report

--- collections_management/collections_management/report/collections_error_report/collections_error_report.py
+++ collections_management/collections_management/report/collections_error_report/collections_error_report.py
@@ -5,6 +5,8 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
+	if not filters.machine_number:
+		filters.machine_number = ""
 	if not filters.site:
 		filters.site = ""
 	if not filters.collected_by:
@@ -12,31 +14,30 @@ def execute(filters=None):
 	if not filters.counted_by:
 		filters.counted_by = ""
 	sqlq = """select 
-			a.creation,
-			a.name,
-			a.machine_number,	
-			a.site,
-			a.meter_reading,	
-			a.previous_reading,
-			a.coins_expected,
-			b.coin_count,
-			b.error,
-			a.owner,
-			b.owner,
-			b.modified
+			a.creation as "Collected On :Date:150",
+			a.name as "Bag No.:Link/Collection Entry:100",
+			a.machine_number as "Machine No.:Link/Asset:100",	
+			a.site as "Site:Link/Warehouse:100",
+			a.meter_reading as "Meter Reading::100",	
+			a.previous_reading as "Previous Reading::100",
+			a.coins_expected as "Expected Coins:Int:100",
+			b.coin_count as "Counted Coins:Int:100",
+			b.error as "Error:Int:100",
+			a.owner as "Collected By::100",
+			b.owner as "Counted By::100",
+			b.modified as "Counted On:Date:150"
 		from `tabCollection Entry` a right join `tabCollection Counting` b 
 		ON a.name = b.collection_entry
-		where a.site like '%{}%'
+		where a.machine_number like '%{}%'
+		and a.site like '%{}%'
 		and a.owner like '%{}%'
 		and b.owner like '%{}%'
 		and a.creation BETWEEN '{}' AND '{}'
-		""".format(filters.site,filters.collected_by,filters.counted_by,filters.from_date,filters.to_date)
-	if filters.machine_number:
-		sqlq+="""and machine_number = '{}'""".format(filters.machine_number)
+		""".format(filters.machine_number,filters.site,filters.collected_by,filters.counted_by,filters.from_date,filters.to_date)
 	
 
 	columns = [
-		"Collected On:Date:150",
+		"Collected On :Date:150",
 		"Bag No.:Link/Collection Entry:100",
 		"Machine No.:Link/Asset:100",
 		"Site:Link/Warehouse:100",
