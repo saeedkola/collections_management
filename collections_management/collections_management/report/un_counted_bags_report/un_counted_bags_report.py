1f57862a4365ec9124f24e88d88e83f01d543a97
Saeed Kola <mohammedsaeedk@gmail.com>
2018-08-10 14:59:21 +0530

    FIXED INDENT ERROR

--- collections_management/collections_management/report/un_counted_bags_report/un_counted_bags_report.py
+++ collections_management/collections_management/report/un_counted_bags_report/un_counted_bags_report.py
@@ -7,28 +7,27 @@ import frappe
 def execute(filters=None):
 	if not filters.site:
 		filters.site =""
-
-    sqlq = """select t1.name as "Bag Number:Link/Collection Entry:100", 
-		t1.machine_number as "Machine Numer:Link/Asset:100",
-		t1.site as "Site: Link/Warehouse: 100",
-		t1.owner as "Collected By::100",
-		t1.creation as "Collected On:Datetime:100"
-		from `tabCollection Entry` t1
-		left join `tabCollection Counting` t2 on t2.collection_entry = t1.name
-		where t2.name IS NULL 
-		and t1.entry_type = "Collection Entry"
-		and t1.creation BETWEEN '{}' AND '{}'
-		and t1.site like '%{}%'
-    """.format(filters.from_date,filters.to_date,filters.site)
+	sqlq = """select t1.name, 
+				t1.machine_number,
+				t1.site,
+				t1.owner,
+				t1.creation
+				from `tabCollection Entry` t1
+				left join `tabCollection Counting` t2 
+				on t2.collection_entry = t1.name
+				where t2.name IS NULL 
+				and t1.entry_type = "Collection Entry"
+				and t1.creation BETWEEN '{}' AND '{}'
+				and coalesce(t1.site,'<NULL>') like '%{}%' """.format(filters.from_date,filters.to_date,filters.site)
     
-    data = frappe.db.sql(sqlq,as_list=1)
+	data = frappe.db.sql(sqlq,as_list=1)
     
-    columns = [
-    "Bag Number:Link/Collection Entry:100",
-    "Machine Number:Link/Asset:100",
-    "Site:Link/Warehouse:100",
-    "Collected By::100",
-    "Collected On:Datetime:100"
+	columns = [
+	    "Bag Number:Link/Collection Entry:100",
+	    "Machine Number:Link/Asset:100",
+	    "Site:Link/Warehouse:100",
+	    "Collected By::100",
+	    "Collected On:Datetime:100"
     ]
 	
 	return columns, data
