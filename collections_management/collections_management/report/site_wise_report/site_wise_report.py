6baa9b021d3163ea03ec6efd58f5cefb9672bb02
Saeed Kola <mohammedsaeedk@gmail.com>
2018-08-14 19:43:21 +0530

    added number of machines on site

--- collections_management/collections_management/report/site_wise_report/site_wise_report.py
+++ collections_management/collections_management/report/site_wise_report/site_wise_report.py
@@ -5,25 +5,33 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	sqlq = """select 
-		a.site as "Site::100",
-		SUM(a.coins_expected) as "Total Expected Coins:Int:100",
-		SUM(b.coin_count) as "Total Counted Coins:Int:100",
-		SUM(b.error) as "Error:Int:100",
-		COUNT(a.machine_number) as "No of Collections::100",
-		AVG(b.coin_count) as "AVG Coins per Collection::150"
-	from `tabCollection Entry` a right join `tabCollection Counting` b 
-	ON a.name = b.collection_entry
-	where a.creation BETWEEN '{}' AND '{}'
-	GROUP BY a.site""".format(filters.from_date,filters.to_date)
+	sqlq = """select q1.*,q2.number from
+			(select a.site,
+			 	SUM(a.coins_expected),
+				SUM(b.coin_count),
+				SUM(b.error),
+				COUNT(a.machine_number),
+				AVG(b.coin_count)
+			from `tabCollection Entry` a 
+			right join `tabCollection Counting` b 
+			ON a.name = b.collection_entry
+			where a.creation BETWEEN '{}' AND '{}'
+			GROUP BY a.site
+			)q1
+			left join
+			(
+			select warehouse,count(name) as number from tabAsset GROUP BY warehouse
+			)q2
+			ON q1.site = q2.warehouse""".format(filters.from_date,filters.to_date)
 
 	columns = [
-		"Site:Link/Warehouse:100",
+		"Site:Link/Warehouse:200",
 		"Total Expected Coins:Int:100",
 		"Total Counted Coins:Int:100",
 		"Error:Int:100",
 		"No of Collections::100",
-		"AVG Coins per Collection::150"
+		"AVG Coins per Collection::100",
+		"No of Machines on Site:Int:100"
 	]
 
 	data = frappe.db.sql(sqlq,as_list=1)
