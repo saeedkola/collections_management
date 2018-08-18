4a9fe934578f7c2aa3717d860f2d65e0b87ab1f3
Saeed Kola <mohammedsaeedk@gmail.com>
2018-08-18 12:40:45 +0530

    fixed currency error

--- collections_management/collections_management/report/collections_report/collections_report.py
+++ collections_management/collections_management/report/collections_report/collections_report.py
@@ -9,16 +9,8 @@ def execute(filters=None):
 		filters.site = ""
 	if not filters.collected_by:
 		filters.collected_by = ""
-	sqlq = """select 
-			creation as "Collected On :Date:150",
-			name as "Bag No.:Link/Collection Entry:100",
-			entry_type,
-			machine_number as "Machine No.:Link/Asset:100",
-			site as "Site:Link/Warehouse:100",
-			meter_reading as "Meter Reading::100",
-			previous_reading as "Previous Reading::100",
-			coins_expected as "Expected Coins::100",
-			owner as "Collected By::100"
+	sqlq = """select creation,name,entry_type,machine_number,site,
+			meter_reading,previous_reading,coins_expected,owner
 		from `tabCollection Entry`
 		where site like '%{}%'
 		and owner like '%{}%'
@@ -30,14 +22,14 @@ def execute(filters=None):
 	
 
 	columns = [
-		"Collected On :Datetime:150",
+		"Collected On:Datetime:150",
 		"Bag No.:Link/Collection Entry:100",
 		"Entry Type ::100",
 		"Machine No.:Link/Asset:100",
 		"Site:Link/Warehouse:100",
 		"Meter Reading::100",
 		"Previous Reading::100",
-		"Expected Coins:Currrency:100",
+		"Expected Coins:Currency:100",
 		"Collected By::100"
 	]
 
