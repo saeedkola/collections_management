0c4172b74bcf6cd6531262e9d6ea3ac9ec6a8c1c
Saeed Kola <mohammedsaeedk@gmail.com>
2018-08-18 13:45:21 +0530

    column changed and bug fixes

--- collections_management/collections_management/report/site_wise_report/site_wise_report.py
+++ collections_management/collections_management/report/site_wise_report/site_wise_report.py
@@ -5,23 +5,24 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	sqlq = """select q1.*,q2.number from
-			(select a.site,
-			 	SUM(a.coins_expected),
-				SUM(b.coin_count),
-				SUM(b.error),
-				COUNT(a.machine_number),
-				AVG(b.coin_count)
+	sqlq = """select q2.warehouse, q1.coins_expected, q1.coin_count, q1.error, q1.no_of_collections, q2.number, q1.avg_count  from
+			(
+			select warehouse,count(name) as number from tabAsset GROUP BY warehouse
+			)q2
+			left join
+			(
+			select a.site,
+			 	SUM(a.coins_expected) as coins_expected,
+				SUM(b.coin_count) as coin_count,
+				SUM(b.error) as error,
+				COUNT(a.machine_number) as no_of_collections,
+				AVG(b.coin_count) as avg_count
 			from `tabCollection Entry` a 
 			right join `tabCollection Counting` b 
 			ON a.name = b.collection_entry
 			where a.creation BETWEEN '{}' AND '{}'
 			GROUP BY a.site
 			)q1
-			left join
-			(
-			select warehouse,count(name) as number from tabAsset GROUP BY warehouse
-			)q2
 			ON q1.site = q2.warehouse""".format(filters.from_date,filters.to_date)
 
 	columns = [
@@ -30,8 +31,9 @@ def execute(filters=None):
 		"Total Counted Coins:Int:100",
 		"Error:Int:100",
 		"No of Collections::100",
-		"AVG Coins per Collection::100",
 		"No of Machines on Site:Int:100"
+		"AVG Coins per Collection::100",
+		
 	]
 
 	data = frappe.db.sql(sqlq,as_list=1)
