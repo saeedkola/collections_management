471a96fd640b33e6b4222b7b62265aea882b548f
Saeed Kola <mohammedsaeedk@gmail.com>
2018-08-18 13:52:36 +0530

    last column error fix

--- collections_management/collections_management/report/site_wise_report/site_wise_report.py
+++ collections_management/collections_management/report/site_wise_report/site_wise_report.py
@@ -5,25 +5,32 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	sqlq = """select q2.warehouse, q1.coins_expected, q1.coin_count, q1.error, q1.no_of_collections, q2.number, q1.avg_count  from
-			(
-			select warehouse,count(name) as number from tabAsset GROUP BY warehouse
-			)q2
-			left join
-			(
-			select a.site,
-			 	SUM(a.coins_expected) as coins_expected,
-				SUM(b.coin_count) as coin_count,
-				SUM(b.error) as error,
-				COUNT(a.machine_number) as no_of_collections,
-				AVG(b.coin_count) as avg_count
-			from `tabCollection Entry` a 
-			right join `tabCollection Counting` b 
-			ON a.name = b.collection_entry
-			where a.creation BETWEEN '{}' AND '{}'
-			GROUP BY a.site
-			)q1
-			ON q1.site = q2.warehouse""".format(filters.from_date,filters.to_date)
+	sqlq = """select 
+	q2.warehouse, 
+	q1.coins_expected, 
+	q1.coin_count, 
+	q1.error, 
+	q1.no_of_collections, 
+	q2.number, 
+	q1.avg_count from
+		(
+		select warehouse,count(name) as number from tabAsset GROUP BY warehouse
+		)q2
+		left join
+		(
+		select a.site,
+		 	SUM(a.coins_expected) as coins_expected,
+			SUM(b.coin_count) as coin_count,
+			SUM(b.error) as error,
+			COUNT(a.machine_number) as no_of_collections,
+			AVG(b.coin_count) as avg_count
+		from `tabCollection Entry` a 
+		right join `tabCollection Counting` b 
+		ON a.name = b.collection_entry
+		where a.creation BETWEEN '{}' AND '{}'
+		GROUP BY a.site
+		)q1
+		ON q1.site = q2.warehouse""".format(filters.from_date,filters.to_date)
 
 	columns = [
 		"Site:Link/Warehouse:200",
