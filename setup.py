8f6d06c101734022fdb18cae03aa3b8f08c3a540
saeedkola <mohammedsaeedk@gmail.com>
2020-12-14 21:10:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-import re, ast
 
-# get version from __version__ variable in collections_management/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
-with open('collections_management/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+# get version from __version__ variable in demo/__init__.py
+from demo import __version__ as version
 
 setup(
 	name='collections_management',
@@ -24,6 +16,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
