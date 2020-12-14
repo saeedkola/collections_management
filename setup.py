231e3c7e96a860a2e1fdf37cc92dcc90a4bfb20e
saeedkola <mohammedsaeedk@gmail.com>
2020-12-14 21:19:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in demo/__init__.py
-from demo import __version__ as version
+from collections_management import __version__ as version
 
 setup(
 	name='collections_management',
